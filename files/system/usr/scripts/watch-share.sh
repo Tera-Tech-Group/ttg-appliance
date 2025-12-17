b67de1021249fd094c9936fa14cf27a4c634abe8
Eric Hernandez <eric@hrn.email>
2025-12-17 15:49:26 -0500

    do not clone empty dirs

--- files/system/usr/scripts/watch-share.sh
+++ files/system/usr/scripts/watch-share.sh
@@ -6,5 +6,5 @@ inotifywait -m -r -e close_write,create "$DIR" |
 while read path action file; do
     logger -t watch-share "Detected $action on $path$file"
     # run your command here, e.g.:
-    rclone --create-empty-src-dirs copy $DIR sharepoint:/General
+    rclone copy $DIR sharepoint:/General
 done
